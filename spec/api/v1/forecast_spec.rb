2b66eb07d08f73eeed811589ad43db9d7139e417
Travis McKinstry <mckinstrytg@gmail.com>
2020-11-05 20:53:48 -0700

    Refactor spec file. Currently have 4 tests which check one API call for various attributes. Each attribute is thoroughly checked (current_weather, daily_weather, hourly_weather)

--- spec/api/v1/forecast_spec.rb
+++ spec/api/v1/forecast_spec.rb
@@ -1,52 +1,98 @@
 require 'rails_helper'
 
-describe 'Forecast API' do
-  it 'can send back a json forecast object by consuming a weather API' do
-
+RSpec.describe 'Forecast API has multiple attributes' do
+  before(:each) do
     get "/api/v1/forecast?location=denver,co"
 
     expect(response).to be_successful
+    @forecast = JSON.parse(response.body)
+  end
+  it "gets only specific keys back within the json['data']['attributes'] hash" do
+    expected = %w(current_weather daily_weather hourly_weather)
+    array = []
+    @forecast['data']['attributes'].keys.each do |attr|
+      if !expected.include?(attr)
+        array << attr
+      end
+    end
+    expect(array).to eq([])
+  end
 
-    forecast = JSON.parse(response.body)
-    # require "pry"; binding.pry
+  it 'can send back a json forecast, current weather, object by consuming a weather API' do
+    expected = %w(datetime sunrise sunset temperature feels_like humidity visibility uvi conditions icon)
+    strings = %w(datetime conditions icon sunrise sunset)
+    integers = %w(temperature feels_like humidity uvi visibility)
+    # expect json attributes[:current_weather] to have these and only these...
+    array = []
+    expected.each do |attr|
+      expect(@forecast['data']['attributes']['current_weather']).to include(attr)
+    end
+    @forecast['data']['attributes']['current_weather'].keys.each do |attr|
+      if !expected.include?(attr)
+        array << attr
+      end
+    end
+    expect(array).to eq([])
 
-    expect(forecast['data']['id']).to be_nil
-    expect(forecast['data']['type']).to eq('forecast')
+    # expect json keys to only be data
+    expect(@forecast.keys).to eq(['data'])
 
-    #current_weather
-    expect(forecast['data']['attributes']).to have_key('current_weather')
-    expect(forecast['data']['attributes']['current_weather']).to have_key('datetime')
-    expect(forecast['data']['attributes']['current_weather']['datetime']).to be_a(String)
+    # expect json['data'] keys to have only 3
+    expect(@forecast['data'].keys).to eq(["id", "type", "attributes"])
+    @forecast['data']['attributes']['current_weather'].each do |key, value|
+      if strings.include?(key)
+        expect(value).to be_a(String)
+      else
+        if value.class == Integer
+          expect(value).to be_an(Integer)
+        else
+          expect(value).to be_a(Float)
+        end
+      end
+    end
+  end
 
-    expect(forecast['data']['attributes']['current_weather']).to have_key('temperature')
-    expect(forecast['data']['attributes']['current_weather']['temperature']).to be_a(Float)
+  it 'can send back a json forecast, daily weather, object by consuming a weather API' do
+    expected = %w(date sunrise sunset max_temp min_temp conditions icon)
 
-    expect(forecast['data']['attributes']['current_weather']).to have_key('feels_like')
-    expect(forecast['data']['attributes']['current_weather']['feels_like']).to be_a(Float)
+    array = []
 
-    expect(forecast['data']['attributes']['current_weather']).to have_key('humidity')
-    expect(forecast['data']['attributes']['current_weather']['humidity']).to_not be_a(String)
+    # has expected keys
+    expected.each do |attr|
+      @forecast['data']['attributes']['daily_weather'].each do |info|
+        expect(info.keys).to include(attr)
+        expect(info['date']).to be_a(String)
+        expect(info['sunrise']).to be_a(String)
+        expect(info['sunset']).to be_a(String)
+        expect(info['max_temp']).to_not be_a(String)
+        expect(info['min_temp']).to_not be_a(String)
+        expect(info['conditions']).to be_a(String)
+        expect(info['icon']).to be_a(String)
+        info.keys.each do |attr|
+          if !expected.include?(attr)
+            array << attr
+          end
+        end
+      end
+    end
 
-    expect(forecast['data']['attributes']['current_weather']).to have_key('uvi')
-    expect(forecast['data']['attributes']['current_weather']['uvi']).to be_a(Float)
+    expect(array).to eq([])
+  end
 
-    expect(forecast['data']['attributes']['current_weather']).to have_key('visibility')
-    expect(forecast['data']['attributes']['current_weather']['visibility']).to_not be_a(String)
-
-    expect(forecast['data']['attributes']['current_weather']).to have_key('conditions')
-    expect(forecast['data']['attributes']['current_weather']['conditions']).to be_a(String)
-
-    expect(forecast['data']['attributes']['current_weather']).to have_key('icon')
-    expect(forecast['data']['attributes']['current_weather']['icon']).to be_a(String)
-
-    # expect(data['attributes']).to have_key('merchant_id')
-    # expect(data['attributes']['merchant_id']).to be_an(Integer)
-    #
-    # expect(data['attributes']).to have_key('created_at')
-    # expect(data['attributes']['created_at']).to be_a(String)
-    #
-    # expect(data['attributes']).to have_key('updated_at')
-    # expect(data['attributes']['updated_at']).to be_a(String)
-
+  it 'can send back a json forecast, hourly weather, object by consuming a weather API' do
+    # expect forecast['data']['attributes']['hourly_weather'] to only...
+    expected = %w(time wind_speed wind_direction conditions icon)
+    array = []
+    @forecast['data']['attributes']['hourly_weather'].each do |day|
+      day.keys.each do |attr|
+        if !expected.include?(attr)
+          array << attr
+        end
+        day.values.each do |val|
+          expect(val).to be_a(String)
+        end
+      end
+    end
+    expect(array).to eq([])
   end
 end

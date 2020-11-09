65b7e1f23803bbd21110a6394f185b28c7e0d638
Travis McKinstry <mckinstrytg@gmail.com>
2020-11-09 11:51:21 -0700

    Refactor search controller in trail

--- app/controllers/api/v1/trails/search_controller.rb
+++ app/controllers/api/v1/trails/search_controller.rb
@@ -5,12 +5,13 @@ module Api
     module Trails
       class SearchController < ApplicationController
         def show
-          if TrailFacade.get_trail_info(params[:location]) == 'location not found'
+          result = TrailFacade.get_trail_info(params[:location])
+          if result == 'location not found'
             self.status = 400
             self.response_body = 'Location not found'
             LocationErrorSerializer.new(params).to_json
           else
-            render json: TrailSerializer.new(TrailFacade.get_trail_info(params[:location]))
+            render json: TrailSerializer.new(result)
           end
         end
       end

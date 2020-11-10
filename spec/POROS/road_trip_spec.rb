69b2b820d2b9c30f73067ae175f23405d9b4cbea
Travis McKinstry <mckinstrytg@gmail.com>
2020-11-09 20:53:25 -0700

    Pass PORO spec for RoadTrip

--- spec/POROS/road_trip_spec.rb
+++ spec/POROS/road_trip_spec.rb
@@ -3,12 +3,19 @@ require 'rails_helper'
 describe RoadTrip do
   it 'Creates a RoadTrip object with specific attributes' do
     data = {
-      start: 1_595_242_800,
-      finish: 22,
+      start: 'denver,co',
+      finish: 'boulder,co',
       eta: 22,
       weather: {temp: 23.5, weather: [{description: 'Nice and good'}]}
     }
+
+    expected = {:temperature=>23.5, :conditions=>"Nice and good"}
     trip = RoadTrip.new(data)
-    require "pry"; binding.pry
+
+    expect(trip).to be_a(RoadTrip)
+    expect(trip.start_city).to eq(data[:start])
+    expect(trip.end_city).to eq(data[:finish])
+    expect(trip.travel_time).to be_a(String)
+    expect(trip.weather_at_eta).to eq(expected)
   end
 end
